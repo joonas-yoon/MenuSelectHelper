8873fa728573febd18ba4d9de5efcd7f7d1a6392
Joonas <joonas.yoon@gmail.com>
2017-12-10 02:06:36 +0900

    v0.2.5 - 디렉토리 구조 변경 - 페이지 추가 - 푸터 CSS 변경

---
+++ js/menuSelector.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+  $("#supportedBrowsers").popup();
+
+  function sleep(ms) {
+    return new Promise(function(resolve){return setTimeout(resolve, ms)});
+  }
+
+  async function doRoulette(funcs, curr, len, speed, endtime){
+    if(speed >= endtime){
+      funcs.onDone();
+      return;
+    }
+      await sleep(speed);
+      funcs.onRun((curr + len -1) % len, curr % len);
+      doRoulette(funcs, curr+1, len, speed + (speed / 20.), endtime);
+  }
+
+  function init(){
+    var pages = $(".step.page");
+    pages.removeClass('transition visible');
+    pages.attr('style', '');
+    $('.ui.label.menu-item').removeClass('basic huge selected yellow blue');
+  }
+
+  function reset(){
+    init();
+    $("#buttonStart").removeClass('disabled loading');
+  }
+
+
+  var randomSpeedMin = 100, randomSpeedMax = 500;
+
+  $('#rangeSpeed').range({
+    min: randomSpeedMin,
+    max: randomSpeedMax,
+    start: (randomSpeedMax + randomSpeedMin) / 2,
+    step: 5,
+    input: '#inputRangeSpeed'
+  });
+
+  var runRoulettePage = function (menuId, menuset){
+    var menuset = menuset || {};
+    var div = $("#page" + menuId);
+    if( !div || !Object.keys(menuset).length ){
+      $("#buttonStart").removeClass('disabled loading');
+      div.removeClass('running');
+      return false;
+    }
+        
+    var menuStr = 'menu' + menuId;
+    div.html('');
+    Object.keys(menuset).forEach(function(m, idx){
+      var item = '<div class="ui label menu-item ' + menuStr + '" id="'+ menuStr +'-'+ idx +'">'+ m +'</div>';
+      div.append(item);
+    });
+
+    console.log(menuset);
+
+    return {
+      animation : 'slide down',
+      onStart: function(){
+        if( div.hasClass('running') ) return;
+        div.addClass('running');
+      },
+      onComplete: function(){
+        if( !div.hasClass('running') ) return;
+
+        var speed = Math.max(10, randomSpeedMax - $("#inputRangeSpeed").val());
+        var endtime = Math.random() * speed + 20;
+        var incTime = Math.min(20, Math.max(50, Math.random()));
+        doRoulette({
+          onRun: function(prev, curr){
+            $('#' + menuStr + '-' + prev).removeClass('huge yellow selected');
+            $('#' + menuStr + '-' + curr).addClass('huge yellow selected');
+          },
+          onDone: function(){
+            var selected = $('.' + menuStr +'.selected');
+            selected.addClass('basic blue');
+            selected.removeClass('yellow');
+            var nextId = menuId + 1;
+            $("#page" + nextId).transition(
+              runRoulettePage(nextId, menuset[selected.html()])
+            );
+          }
+        },
+          0, Object.keys(menuset).length, incTime, endtime
+        );
+      }
+    };
+  };
+
+  function startRoullette(menus){
+    $("#page1").transition(runRoulettePage(1, menus));
+  }
+
+  $("#buttonStart").on('click', function(evt){
+    var e = evt || window.event;
+    //IE9 & Other Browsers
+    if (e.stopPropagation) { e.stopPropagation(); }
+    //IE8 and Lower
+    else { e.cancelBubble = true; }
+    e.preventDefault();
+
+    init();
+    $("#buttonStart").addClass('disabled loading');
+    $.getJSON("./menus.json", startRoullette);
+  });
+});
